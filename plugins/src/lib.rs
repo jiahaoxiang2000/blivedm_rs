2551865fd7622b4e12767f2e5e32efc84e656691
isomo <jiahaoxiang2000@gmail.com>
2025-05-23 09:52:42 +0800

    feat: implement TTS handler for Danmaku text-to-speech functionality on local (TTS service to, transform danmu to voice #1)

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -1,10 +1,17 @@
 pub mod terminal_display;
+pub mod tts;
+
 use client::scheduler::EventHandler;
 use std::sync::Arc;
 
 /// Helper to create the handler as Arc<dyn EventHandler>
 pub fn terminal_display_handler() -> Arc<dyn EventHandler> {
     Arc::new(terminal_display::TerminalDisplayHandler)
+}
+
+/// Helper to create the TTS handler as Arc<dyn EventHandler>
+pub fn tts_handler(tts_command: String, tts_args: Vec<String>) -> Arc<dyn EventHandler> {
+    Arc::new(tts::TtsHandler::new(tts_command, tts_args))
 }
 
 #[cfg(test)]
